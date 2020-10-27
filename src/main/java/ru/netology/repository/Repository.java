d0cca5456322f80423550a34bdcdf9e43db14191
Shamiltestgrid <60189855+Shamiltestgrid@users.noreply.github.com>
2020-10-27 12:35:24 +0300

    Update Repository.java

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -1,25 +1,50 @@
 package ru.netology.repository;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.netology.domain.OfferToFly;
+import ru.netology.domain.Offer;
+import ru.netology.exception.NotFoundException;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Repository {
-    OfferToFly[] offers = new OfferToFly[0];
+public class OfferRepository {
+    private Offer[] offers = new Offer[0];
 
-    public void save(OfferToFly offer) {
+    public void save(Offer offer) {
         int length = offers.length + 1;
-        OfferToFly[] tmp = new OfferToFly[length];
+        Offer[] tmp = new Offer[length];
         System.arraycopy(offers, 0, tmp, 0, offers.length);
         tmp[tmp.length - 1] = offer;
         offers = tmp;
     }
 
-    public OfferToFly[] getAll() {
+    public Offer findById(int id) {
+        for (Offer offer : offers) {
+            if (offer.getId() == id) {
+                return offer;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException("Element with id: " + id + " not found!");
+        } else {
+            int length = offers.length - 1;
+            Offer[] tmp = new Offer[length];
+            int index = 0;
+            for (Offer item : offers) {
+                if (item.getId() != id) {
+                    tmp[index] = item;
+                    index++;
+                }
+            }
+            offers = tmp;
+        }
+    }
+
+    public Offer[] getAll() {
         return offers;
     }
 }
